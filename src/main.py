d3685ea43afae0d487ad997562a6e7628a14f9aa
Felipe Canales <felipe.canales.213@gmail.com>
2024-07-25 21:07:09 -0400

    working db

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from src.database import personas
+
 app = FastAPI()
 
 
@@ -8,5 +10,21 @@ def read_root() -> dict[str, str]:
     return {"Hello": "World"}
 
 
-def base_datos() -> None:
-    return
+@app.post("/persona")
+def crear_persona() -> dict[str, str]:
+    result = personas.insert_one(
+        {
+            "nombre": "Juanito Perez",
+            "introduccion": "Hola me llamo Juanito Perez",
+            "titulo": "Zopenco",
+        }
+    )
+    return {"id": result.inserted_id}
+
+
+@app.get("/persona")
+def obtener_persona() -> dict[str, str]:
+    result: dict[str, str] | None = personas.find_one()
+    if result is not None:
+        return {"nombre": result["nombre"], "titulo": result["titulo"]}
+    return {}
